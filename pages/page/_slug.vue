9b569e7ff82179f6a54e9cd626d70b423b34b161
Aleksej <git@aleksejdix.com>
2019-11-17 16:22:00 +0100

    pages

---
+++ pages/page/_slug.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{$data}}</div>
+</template>
+
+<script>
+export default {
+ async asyncData({ params, app, payload, route, store }) {
+    const post = await import("~/content/pages/page/" + params.slug + ".json");
+    return post;
+  },
+}
+</script>
+
+<style>
+
+</style>
